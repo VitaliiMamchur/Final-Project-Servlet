15274e68dfa14bb49a7f27926eeb9d939e45fdc8
Vitalii Mamchur <vitaliiimamchur@gmail.com>
2020-02-18 14:33:00 +0200

    filters added

---
+++ src/main/java/ua/mamchur/servletproject/servlet/filter/MasterFilter.java
@@ -0,0 +1,32 @@
+package ua.mamchur.servletproject.servlet.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/masterlist"})
+public class MasterFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        final HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        HttpSession session = request.getSession();
+        String role = (String) session.getAttribute("role");
+        if (!(role.equals("ROLE_MASTER"))) {
+            servletRequest.getServletContext().getRequestDispatcher("/WEB-INF/error.jsp").forward(request, response);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
